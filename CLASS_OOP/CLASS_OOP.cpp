0a8d5b97e9135a3943d91a9480eb980e801a48fc
Jantopthegreat <fauzangriya@gmail.com>
2023-04-12 10:38:04 +0700

    memasukan input MK(matakuliah) beserta displaynya

--- CLASS_OOP/CLASS_OOP.cpp
+++ CLASS_OOP/CLASS_OOP.cpp
@@ -32,4 +32,12 @@ private:
 	string kode;
 	string namaMK;
 	int sks;
-}
+public:
+	void inputMK() {
+		cout << "\n\nMasukan Kode Matakuliah : ";
+		cin >> kode;
+		cout << "Masukan Nama Matakuliah : ";
+		cin >> namaMK;
+		cout << "Masukan SKS : ";
+		cin >> sks;
+	}
